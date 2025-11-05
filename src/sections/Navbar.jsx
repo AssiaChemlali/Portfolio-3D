12e200d90667752bd7898d190ad0dbd085e7c0dc
AssiaChemlali <chemlali.assia@gmail.com>
2025-11-05 11:50:17 +0100

    fix navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import Button from '../components/Button'
+import React, { useEffect, useState } from 'react'
 import { navLinks } from '../data'
 const Navbar = () => {
+    const[scrolled,setScrolled]=useState(false)
+    useEffect(()=>{
+       const handleScroll=()=>{
+        const isScrolled=window.scrollY > 10
+        setScrolled(true)
+       }
+
+    window.addEventListener('scroll',handleScroll)
+    return  ()=>window.removeEventListener('scroll',handleScroll)
+
+    },[])
+
   return (
+  
+    <header className={`fixed w-full  left-1/2 py-5 px-5 md:px-20 -translate-1/2 transition-all duration-300 ease-in-out z-100 ${scrolled ? 'top-0 bg-black':'md:top-10 top-0 bg-transparent'}`}>
 
-    <header className='flex flex-col w-full '>
-      <div className='flex flex-col md:flex-row gap-5  items-center  h-[20vh] justify-between px-20 '>
-
-        <div className=''>
-          <a href="#hero" className='text-bold text-2xl text-white-50 font-bold'>Adrian | JSM</a>
-        </div>
-
+      <div className='mx-auto flex items-center   justify-between '>
+        
+        <a href="#hero" className='text-semibold text-xl  md:text-2xl text-white-50 font-bold transition-transform duration-300 hover:scale-105'>Adrian | JSM</a>
+        
         {/* desktop menu */}
         <nav className='hidden lg:flex'>
           <ul className='flex flex-row items-center gap-5 text-white-50 hover:text-white '>
             {
               navLinks.map((link)=>(
-                <li key={link.name}>
+                <li key={link.name} className=''>
                   <a href={link.link}>
                     <span>{link.name}</span>
-                    <span></span>
+                    <span className=''></span>
                   </a>
                 </li>
               ))
